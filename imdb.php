1073da43484f63db88f7cd22cfe09a95b4488195
NaldiG <67630281+NaldiG@users.noreply.github.com>
2021-04-14 15:42:02 +0200

    Add files via upload

--- imdb.php
+++ imdb.php
@@ -3,20 +3,32 @@
 $random = rand(1, 1000);
 $file = fopen("imdb_top_1000.csv", "r");
 $line = "";
-$counter = 0;
+$index = 0;
 
 while($line = fgets($file)){
-    if($counter == $random){
+    if($index == $random){
         break;
     }
-    $counter++;
+    $index++;
 }
 
 $movie = explode(",", $line);
+
+//Each poster url contains 3 commas and since they are separated in the line above I have to concatenate the first 4 array entries to get the full poster url.
+
 $poster = $movie[0] . "," . $movie[1] . "," . $movie[2] . "," . $movie[3];
 $title = $movie[4];
 $year = $movie[5];
-$rating = $movie[9];
+
+//The genre column comes before the rating column and it can contain several genres separated by commas.
+//Because of this, instead of displaying the rating the program would display the second genre of the movie.
+//I start at the index where the rating is supposed to be if there were just one genre and I increase the index by one each time I dont get a boolean or int.
+
+$index = 9;
+while(floatval($movie[$index]) == 0 || intval($movie[$index]) == 0){
+    $index++;
+}
+$rating = $movie[$index];
 
 echo("<table border='1'>
       <tr>
